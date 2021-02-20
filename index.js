ad45b9078c23ebc31106abdf90b939e728dce19c
Vineet Singh <vwsingh@outlook.com>
2021-02-20 11:24:35 +1100

    week 1, http server with request filtering

--- index.js
+++ index.js
@@ -1,14 +1,41 @@
 const http =  require('http');
 const hostname = 'localhost';
 const port = 3000;
+const fs = require('fs');
+const path = require('path');
 
 const server = http.createServer((req,res)=>{
-    console.log("request headers: ");
+    console.log("Request for " + req.url + " by method " + req.method);
     console.log(req.headers);
-
-    res.statusCode=200; 
+    if (req.method == 'GET'){
+        let fileUrl;
+        if (req.url == '/') fileUrl='./index.html';
+        else fileUrl=req.url;
+        let filePath = path.resolve('./public'+fileUrl);
+        const fileExt= path.extname(filePath);
+        if (fileExt == '.html'){
+            fs.stat(filePath, (exists)=>{
+                if (!exists) {
+                    res.statusCode = 404;
+                    res.setHeader('Content-Type','text-html');
+                    res.end('<html> <body> <h1> File not found - Error 404 </h1> </body> </html>');
+                }
+                else{
+                    res.statusCode = 200;
+                    res.setHeader('Content-Type','text-html');
+                    fs.createReadStream(filePath).pipe(res);
+                }
+            });
+        }
+        else{
+            res.statusCode = 404;
+            res.setHeader('Content-Type','text-html');
+            res.end('<html> <body> <h1> File not an HTML file - Error 404 </h1> </body> </html>');
+        }
+    }
+    res.statusCode = 404;
     res.setHeader('Content-Type','text-html');
-    res.end('<html> <body> <h1> hello world from vinnie singh node.js </body> </html>');
+    res.end('<html> <body> <h1> Request method not supported - Error 404 </h1> </body> </html>');
 });
 
 server.listen(port, hostname, ()=> {
