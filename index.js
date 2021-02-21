49c743353c4618c1664e5fe5597ed3394a608562
Vineet Singh <vwsingh@outlook.com>
2021-02-22 10:12:01 +1100

    express serving static files

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const http =  require('http');
+const morgan = require('morgan');
+
 const hostname = 'localhost';
 const port = 3000;
 const fs = require('fs');
@@ -7,10 +9,12 @@ const path = require('path');
 
 /* use express */
 const app=express();
+app.use(morgan('dev'));
+app.use(express.static(__dirname+ '/public'));
 
 app.use((req,res,next)=>{
     console.log(req.headers);
-    res.statusCode = 404;
+    res.statusCode = 200;
                     res.setHeader('Content-Type','text-html');
                     res.end('<html> <body> <h1> This is an express server</h1> </body> </html>');
     
